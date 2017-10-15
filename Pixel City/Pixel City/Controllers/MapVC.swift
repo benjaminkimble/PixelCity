88bf66635a7862e0bd58e65299f50ef55f5b513b
Benjamin Kimble <benjaminroykimble@gmail.com>
2017-10-15 10:03:10 -0600

    Initialized Project and Created CocoaPods Workspace
    - Initialized Project
    - Initialized CocoaPods Workspace
    - Added Alamofire Pod
    - Added AlamofireImage Pod
    - Updated Build Settings for Pods

---
+++ Pixel City/Pixel City/Controllers/MapVC.swift
@@ -0,0 +1,25 @@
+//
+//  MapVC.swift
+//  Pixel City
+//
+//  Created by Benjamin Kimble on 10/15/17.
+//  Copyright © 2017 Benjamin Kimble. All rights reserved.
+//
+
+import UIKit
+
+class MapVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
